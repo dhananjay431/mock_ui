de4f9f1fa0a18f9243efca7776f0882de81720ee
dbel431 <dbel431@gmail.com>
2026-05-21 13:54:39 +0530

    add modal to pdf data

--- src/app/pages/main/expenses/expenses.ts
+++ src/app/pages/main/expenses/expenses.ts
@@ -8,9 +8,46 @@ import { Hero } from '../../../hero';
   styleUrl: './expenses.scss',
 })
 export class Expenses {
+  receiptZoom = 100;
+  receiptZoomMin = 50;
+  receiptZoomMax = 200;
+  receiptZoomStep = 25;
+
   constructor(private hero: Hero) {}
 
   showToast(message: string, type: string) {
     this.hero.showToast(message, type);
+  }
+
+  openReceiptModal() {
+    this.receiptZoom = 100;
+  }
+
+  closeReceiptModal() {
+    this.receiptZoom = 100;
+  }
+
+  zoomIn() {
+    if (this.receiptZoom < this.receiptZoomMax) {
+      this.receiptZoom += this.receiptZoomStep;
+    }
+  }
+
+  zoomOut() {
+    if (this.receiptZoom > this.receiptZoomMin) {
+      this.receiptZoom -= this.receiptZoomStep;
+    }
+  }
+
+  zoomReset() {
+    this.receiptZoom = 100;
+  }
+
+  zoomFit() {
+    this.receiptZoom = 100;
+  }
+
+  getZoomTransform(): string {
+    return `scale(${this.receiptZoom / 100})`;
   }
 }
